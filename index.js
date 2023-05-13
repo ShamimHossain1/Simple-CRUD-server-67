115cb957a01b26d183ef859946655e7e2888469b
ShamimHossain1 <beastb697@gmail.com>
2023-05-14 03:11:59 +0600

    delete methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
@@ -30,12 +30,28 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
 
-        app.post('/users', async(req,res)=>{
+        app.get('/users', async (req, res) => {
+
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+
+        })
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user)
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id: new ObjectId(id)};
+            
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
         await client.db("admin").command({ ping: 1 });
@@ -57,7 +73,7 @@ app.listen(port, () => {
 })
 
 
-// functions 
+// functions
 
 // async function run(){
 
